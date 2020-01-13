073f23d368cc3d0c094b353b51c21324c971ce4d
d-jskim <d.unicornhybrid@gmail.com>
2020-01-13 20:40:02 +0900

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		int num = 123;
+		
+		Scanner kb = new Scanner(System.in);
+		int input = kb.nextInt();
+		
+		if(input == num) {
+			System.out.print("Strings match");
+		}else {
+			System.out.print("Strings do not match");
+		}
+		kb.close();
 	}
 }
